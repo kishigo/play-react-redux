568bf7599475b86dce1ffa214214b401ea266369
kishigo <kelvin.ishigo@gmail.com>
2017-10-05 09:39:35 -0700

    Update ShoppingTypes Add support (untested) for delete actions in shopping

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -4,7 +4,7 @@
  * Copyright (c) 2017 Kelvin Ishigo
  * Licensed under the MIT License. See LICENSE file in the project root for full license information.
  */
-// Types specific to recipe SPA
+// Types specific to recipe SPA...Convention is to work verb + object
 export const RecipeTypes = {
 	INGEST_RECIPE: 'INGEST_RECIPE',
 	DELETE_RECIPE: 'DELETE_RECIPE',
@@ -13,9 +13,13 @@ export const RecipeTypes = {
 	DISPLAY_RECIPE: 'DISPLAY_RECIPE',
 	HIGHLIGHT_RECIPE: 'HIGHLIGHT_RECIPE'
 };
-
+/**
+ * Important that strings must be unique across all <action>Types
+ * @type {{ADD_ITEM: string, DELETE_ITEM: string, DELETE_COMPLETED_ITEMS: string, COMPLETE_ITEM: string}}
+ */
 export const ShoppingTypes = {
-	ADD_ITEM: 'ADD_ITEM',
-	DELETE_ITEM: 'DELETE_ITEM',
-	COMPLETE_ITEM: 'COMPLETE_ITEM'
+	ADD_ITEM: 'ADD_SHOPPING_ITEM',
+	DELETE_ITEM: 'DELETE_SHOPPING_ITEM',
+	DELETE_COMPLETED_ITEMS: 'DELETE_COMPLETED_SHOPPING_ITEMS',
+	COMPLETE_ITEM: 'COMPLETE_SHOPPING_ITEM'
 };
--- src/reducers/shopping.js
+++ src/reducers/shopping.js
@@ -5,10 +5,16 @@
  * Licensed under the MIT License. See LICENSE file in the project root for full license information.
  */
 import {ShoppingTypes} from '../actions/ActionTypes';
-
+/**
+ * applies actions on the slice of state which is the array of shoppingItems
+ * @param state - array of shoppingItems
+ * @param action - supported actions for this state slice
+ * @returns {*} - new state or unchanged original
+ */
 const shoppingItems = (state = [], action) => {
 	switch (action.type) {
 		case ShoppingTypes.ADD_ITEM:
+			// This defines the actual shape of the state item in the array
 			return [
 				...state,
 				{
@@ -21,12 +27,16 @@ const shoppingItems = (state = [], action) => {
 				}
 			];
 		case ShoppingTypes.COMPLETE_ITEM:
+			// find the item and toggle the completed flag
 			return state.map(item =>
 				(item.id === action.id)
 					? {...item, completed: !item.completed}
 					: item
 			);
 		case ShoppingTypes.DELETE_ITEM:
+			return state.filter((item) => (item.id !== action.id));
+		case ShoppingTypes.DELETE_COMPLETED_ITEMS:
+			return state.filter((item) => (!item.completed));
 		default:
 			return state;
 	}
